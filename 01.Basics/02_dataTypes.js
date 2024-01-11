05c58b57d033e6de76b34107cf2bd15f83c55f81
Shivam Suthar <shivam.suthar@saeculumsolutions.com>
2024-01-11 16:37:11 +0530

    datatypes

---
+++ 01.Basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";
+const { log } = require("console");
+
+ // treat all JS code as newer version of JS
+
+ // alert(3 + 3); // we are using node.js
+
+ let name = "shivam"; // it is string
+ let age = 18 // it is number
+ let isLoggedIn = true / false // it is boolen
+
+// premitive data-type
+
+// number => 2 to power 53
+// bigint => for bigger number 
+// string => it is text
+// boolen => simple true or false 
+// null => standalone value also, it is not undefind it is empty
+// undefind =>
+// symbol => to find unique
+
+// object
+
+console.log(typeof "shivam"); 
