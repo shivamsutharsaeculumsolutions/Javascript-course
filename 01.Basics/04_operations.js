ee899aabed8f34ff1aad9014dda12eacb35e9281
Shivam Suthar <shivam.suthar@saeculumsolutions.com>
2024-01-11 18:42:32 +0530

    operations

---
+++ 01.Basics/04_operations.js
@@ -0,0 +1,26 @@
+let value = 3;
+let negValue = -value;
+
+console.log(value);
+console.log(negValue);
+
+let str1 = "hello";
+let str2 = "shivam";
+
+let str3 = str1 + " " + str2;
+
+console.log(str3);
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
