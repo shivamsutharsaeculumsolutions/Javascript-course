9d68a2f89d19cd7d95256cc01077f6ef1db23cd9
Shivam Suthar <shivam.suthar@saeculumsolutions.com>
2024-01-11 18:07:09 +0530

    conversion of data types

---
+++ 01.Basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "undefined";
+
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// undefined => NaN
+
+
+let isLoggedIn = 0;
+
+let boolenIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof boolenIsLoggedIn);
+// console.log(boolenIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "string" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log(typeof (someNumber));
+console.log(someNumber);
